1a5d62bd59d6c7510cc11334ac6ccf3119754129
박예람 <rhdwn8885@naver.com>
2021-04-06 17:26:39 +0900

    소프트웨어공학 과제

---
+++ 4주차-실습1.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char s1[20] = " 9th Symphony";
+	char s2[40] = "Beethoven";
+
+	// 두 배열을 활용하여 Beethoven 9th Symphony 문자열을 배열 s2에 저장
+	int pointer = 0;
+	int i = 0;
+
+	for (int i = 0; i < sizeof(s2); i++)
+	{
+		if (s2[i] == '\0')
+		{
+			s2[i] = s1[pointer];
+			pointer++;
+		}
+	}
+
+	printf("%s\n", s2);
+	return 0;
+}

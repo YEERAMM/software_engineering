053b59acc42ba1b1ee16e8d7ae385b14129fcc9a
박예람 <rhdwn8885@naver.com>
2021-04-06 18:14:48 +0900

    완료

--- 4주차-실습1.cpp
+++ 4주차-실습1.cpp
@@ -3,10 +3,10 @@
 
 int main()
 {
-	char s1[20] = " 9th Symphony";
-	char s2[40] = "Beethoven";
+	char s1[20] = " 5th Symphony";
+	char s2[40] = "Tchaikovsky";
 
-	// 두 배열을 활용하여 Beethoven 9th Symphony 문자열을 배열 s2에 저장
+	// 두 배열을 활용하여 Tchaikovsky 5th Symphony 문자열을 배열 s2에 저장
 	int pointer = 0;
 	int i = 0;
 
